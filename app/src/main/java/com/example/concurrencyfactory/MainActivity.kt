cd6fdb0534c51b782887c2114a78d9e90a1d9f71
Gleb Trusov <gleb.trusow@yandex.ru>
2023-12-22 20:05:47 +0300

    Suspend функция delay() не блокирует главный поток, выполняясь на главном потоке (Dispatchers.Main)

--- app/src/main/java/com/example/concurrencyfactory/MainActivity.kt
+++ app/src/main/java/com/example/concurrencyfactory/MainActivity.kt
@@ -1,19 +1,47 @@
 package com.example.concurrencyfactory
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.concurrencyfactory.ui.theme.ConcurrencyFactoryTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+
+    private val scope = CoroutineScope(Dispatchers.Main)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("MyTest", "onCreate 1")
+        setContent { UI() }
+
+        val job = scope.launch(start = CoroutineStart.LAZY) {
+            delay(3000)
+            Log.d("MyTest", "coroutine ${Thread.currentThread().name}")
+        }
+
+        job.start()
+
+        Log.d("MyTest", "onCreate 2")
+    }
+}
+
+@Composable
+private fun UI() {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        CircularProgressIndicator()
     }
 }
